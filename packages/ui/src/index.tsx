e953aed07506bf313decb6ebbd28c4266c60ba1b
SAKSAUD7 <saksaud7411@gmail.com>
2025-11-26 03:19:09 +0530

    fix: resolve merge conflict markers in ui/index.tsx

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -13,11 +13,9 @@ export { BouncyButton } from "./components/BouncyButton";
 export { ScrollReveal } from "./components/ScrollReveal";
 export { SectionDivider } from "./components/SectionDivider";
 export { VideoSection } from "./components/VideoSection";
-<<<<<<< HEAD
 export { ImageCarousel } from "./components/ImageCarousel";
 export { Marquee } from "./components/Marquee";
-=======
->>>>>>> ae298bdb839d28f6b8769d78d13513cb682514bb
+
 
 // Utilities
 export { cn } from "./lib/utils";
